804ea6f6aa6a2d98bfef71cf098b6f0929ed4901
Mirthe <65893734+M1rthe@users.noreply.github.com>
2020-11-18 10:52:27 +0100

    Made a Blend project and put in in CMakeLists.txt and when I build it works

--- Blend/myentity.cpp
+++ Blend/myentity.cpp
@@ -22,8 +22,9 @@ void MyEntity::update(float deltaTime)
 	// ###############################################################
 	// Rotate
 	// ###############################################################
+	/*
 	this->rotation.z += HALF_PI * deltaTime; // 90 deg/sec
 	if (this->rotation.z > TWO_PI) {
 		this->rotation.z -= TWO_PI;
-	}
+	}*/
 }
